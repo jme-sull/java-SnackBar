16e6de3053ea6eb9ad33d32f5dab9a9313630ec1
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-07-06 16:39:51 -0400

    created snack class

---
+++ src/snackbar/Main.java
@@ -0,0 +1,17 @@
+package snackbar;
+
+public class Main 
+{
+	private static void workwithData()
+
+	{
+		String hello = "hello";
+		System.out.println(hello);
+
+	}
+
+	public static void main(String args[])
+	{
+		workwithData();
+	}
+}
---
+++ src/snackbar/Snack.java
@@ -0,0 +1,75 @@
+package snackbar;
+
+public class Snack
+{
+	//fields
+	private static int maxId = 0;
+
+	private int id;
+	private String name;
+	private int quantity;
+	private double cost;
+	private int vendingMachineId;
+
+	//constructor
+
+	public Snack(String name, int quantity, double cost, int vendingMachineId)
+	{
+		maxId++;
+		id = maxId;
+
+		this.name = name;
+		this.quantity = quantity;
+		this.cost = cost;
+		this.vendingMachineId = vendingMachineId;
+
+	}
+
+	//methods
+	public int getId()
+	{
+		return id;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public double getCost()
+	{
+		return cost;
+	}
+	public void setCost(double cost)
+	{
+		this.cost = cost;
+	}
+	public int getVendingMachineId()
+	{
+		return vendingMachineId;
+	}
+	public void setVendingMachineId(int vendingMachineId)
+	{
+		this.vendingMachineId = vendingMachineId;
+	}
+	public int getQuantity()
+	{
+		return quantity;
+	}
+	public void addQuantity(int quantity)
+	{
+		this.quantity = this.quantity + quantity;
+	}
+	public void buySnack(int quantity)
+	{
+		this.quantity = this.quantity - quantity;
+	}
+	public double getTotal(int quantity)
+	{
+		double total = this.cost * quantity;
+		return total;
+	}
+
+}
